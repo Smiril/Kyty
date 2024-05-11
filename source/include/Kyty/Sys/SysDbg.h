18bcdeb6c5a596362c74040a9520881f0514e62d
Smiril <Smiril@users.noreply.github.com>
2024-05-11 21:53:00 +0200

    Update SysDbg.h
    add macOS support

--- source/include/Kyty/Sys/SysDbg.h
+++ source/include/Kyty/Sys/SysDbg.h
@@ -3,6 +3,7 @@
 
 #include "Kyty/Core/Common.h"
 #include "Kyty/Sys/Linux/SysLinuxDbg.h"     // IWYU pragma: export
+#include "Kyty/Sys/MacOS/SysMacOSDbg.h"     // IWYU pragma: export
 #include "Kyty/Sys/Windows/SysWindowsDbg.h" // IWYU pragma: export
 
 #endif /* INCLUDE_KYTY_SYS_SYSDBG_H_ */
