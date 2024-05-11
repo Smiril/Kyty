b645002bad11c1214c577c4b494282181021467e
Smiril <Smiril@users.noreply.github.com>
2024-05-11 21:46:37 +0200

    Update SysVirtual.h
    add macOS support

--- source/include/Kyty/Sys/SysVirtual.h
+++ source/include/Kyty/Sys/SysVirtual.h
@@ -3,6 +3,7 @@
 
 #include "Kyty/Core/Common.h"
 #include "Kyty/Sys/Linux/SysLinuxVirtual.h"     // IWYU pragma: export
+#include "Kyty/Sys/MacOS/SysMacOSVirtual.h"     // IWYU pragma: export
 #include "Kyty/Sys/Windows/SysWindowsVirtual.h" // IWYU pragma: export
 
 #endif /* INCLUDE_KYTY_SYS_SYSVIRTUAL_H_ */
