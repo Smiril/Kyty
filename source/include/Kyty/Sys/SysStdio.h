bfc71fecc1e43776a87f90ed1fd9b1504c11ab24
Smiril <Smiril@users.noreply.github.com>
2024-05-11 21:50:32 +0200

    Update SysStdio.h
    add macOS support

--- source/include/Kyty/Sys/SysStdio.h
+++ source/include/Kyty/Sys/SysStdio.h
@@ -3,6 +3,7 @@
 
 #include "Kyty/Core/Common.h"
 #include "Kyty/Sys/Linux/SysLinuxStdio.h"     // IWYU pragma: export
+#include "Kyty/Sys/MacOS/SysMacOSStdio.h"     // IWYU pragma: export
 #include "Kyty/Sys/Windows/SysWindowsStdio.h" // IWYU pragma: export
 
 #endif /* INCLUDE_KYTY_SYS_SYSSTDIO_H_ */
