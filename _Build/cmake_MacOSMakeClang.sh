a089c3cdf70096abc0c2a49a867f4d03f70b1784
Smiril <Smiril@users.noreply.github.com>
2024-05-12 18:39:44 +0200

    compiler fix in build scripts

--- _Build/cmake_MacOSMakeClang.sh
+++ _Build/cmake_MacOSMakeClang.sh
@@ -7,19 +7,19 @@ do
     case $opt in
 	"Debug")
 	    DirName=_DebugMacOSMakeClang
-	    Options="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=g++ -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=_bin ../../source"
+	    Options="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -D CMAKE_BUILD_TYPE=Debug -D CMAKE_INSTALL_PREFIX=_bin ../../source"
 		;;
 	"Debug Final")
 	    DirName=_DebugFinalMacOSMakeClang
-	    Options="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=g++ -D CMAKE_BUILD_TYPE=Debug -D KYTY_FINAL=1 -D CMAKE_INSTALL_PREFIX=_bin ../../source"
+	    Options="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -D CMAKE_BUILD_TYPE=Debug -D KYTY_FINAL=1 -D CMAKE_INSTALL_PREFIX=_bin ../../source"
 		;;
 	"Release")
 	    DirName=_ReleaseMacOSMakeClang
-	    Options="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=g++ -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=_bin ../../source"
+	    Options="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=_bin ../../source"
 		;;
 	"Release Final")
 	    DirName=_ReleaseFinalMacOSMakeClang
-	    Options="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=g++ -D CMAKE_BUILD_TYPE=Release -D KYTY_FINAL=1 -D CMAKE_INSTALL_PREFIX=_bin ../../source"
+	    Options="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -D CMAKE_BUILD_TYPE=Release -D KYTY_FINAL=1 -D CMAKE_INSTALL_PREFIX=_bin ../../source"
 		;;
     esac
     break
